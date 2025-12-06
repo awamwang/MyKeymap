65e5ae12219728e3b90e0e04683602d1166a3872
awamwang <wm.keepgoing@gmail.com>
2025-12-06 22:14:40 +0800

    fix: config-ui修复依赖问题

--- config-ui/vite.config.ts
+++ config-ui/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     }),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
     vuetify({
-      autoImport: false,
+      autoImport: true,
       styles: {
         configFile: 'src/styles/settings.scss',
       },
